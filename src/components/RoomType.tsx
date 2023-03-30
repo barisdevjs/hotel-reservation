be32460f5b8beb2ddb65cbd71061f7c5e4fe3f24
Barış SAVAŞ <barissavas17@gmail.com>
2023-03-31 00:47:03 +0300

    Room details part 2

--- src/components/RoomType.tsx
+++ src/components/RoomType.tsx
@@ -1,15 +1,21 @@
-import { Avatar, Card, Col, Form, Radio, Row, Divider, Space,Typography } from 'antd'
-import React, { useMemo } from 'react';
+import { Avatar, Card, Col, Form, Radio, Row, Divider, Space, Typography } from 'antd'
+import  { useMemo } from 'react';
 import { HotelProps, RoomI } from '../utils/types';
 import { Dayjs } from 'dayjs';
 const { Meta } = Card;
 
 
 function RoomType({ data, setData, updateFields }: HotelProps) {
+    let rooms: RoomI[] = [];
 
-    const rooms = data.room_type as RoomI[];
+    if (Array.isArray(data.room_type)) {
+        rooms = data.room_type;
+    }
 
-    console.log(data);
+    const onChange1 = (e: any) => {
+        const room = rooms.find(room => room.id === e.target.value);
+        updateFields({ selectedRoom: room });
+    };
 
     function calculateDays(start: Dayjs, end: Dayjs): number {
         return end.diff(start, 'day')
@@ -24,14 +30,17 @@ function RoomType({ data, setData, updateFields }: HotelProps) {
 
 
     return (
-        <Form.Item label="Room Type Selection" className='font-label' style={{marginInline:'auto'}}>
-            <Radio.Group >
-                <Row gutter={[64, 32]} align='top' justify='space-evenly' style={{marginInline:'auto'}}>
-                    {rooms.map((room) =>
+        <Form.Item label="Room Type Selection" className='font-label' style={{ marginInline: 'auto' }}>
+            <Radio.Group onChange={onChange1}>
+                <Row gutter={[64, 32]} align='top' justify='space-evenly' style={{ marginInline: 'auto' }}>
+                    {rooms.map((room: RoomI) =>
                         <Col key={room.id}>
-                            <Radio value={room.id}>
+                            <Radio value={room.id} >
                                 <Card title={room.title} bordered={false} size='small'
-                                    style={{ width: 300 }} hoverable
+                                    style={{
+                                        width: 300, boxShadow: data.selectedRoom.id === room.id ? '0 0 5px 3px rgba(0, 0, 0, 0.2)' : 'none'
+                                    }}
+                                    hoverable
                                     cover={
                                         <img
                                             alt={room.title}
@@ -40,14 +49,25 @@ function RoomType({ data, setData, updateFields }: HotelProps) {
                                     }>
                                     <Row align='top' justify='space-between' style={{ width: '100%' }}>
                                         <Col>
-                                        <Typography.Text strong>{data.parents}</Typography.Text>
-                                        <Typography.Text>adults</Typography.Text> </Col>
-                                        {data.children && <Col>{data.children} children</Col>}
-                                        <Col>{totalDays} days</Col>
+                                            <Typography.Text strong>{data.parents}</Typography.Text>
+                                            &nbsp;
+                                            <Typography.Text type='secondary'>adults</Typography.Text> </Col>
+                                        {data.children && <Col>
+                                            <Typography.Text strong>{data.children}</Typography.Text>
+                                            &nbsp;
+                                            <Typography.Text type='secondary'>children</Typography.Text>
+                                        </Col>}
+                                        <Col>
+                                            <Typography.Text strong>{totalDays}</Typography.Text>&nbsp;
+                                            <Typography.Text type='secondary'>days</Typography.Text>
+                                        </Col>
                                     </Row>
                                     <Divider dashed>Details</Divider>
                                     <Meta
-                                        title={<Row justify='space-between'><Col>Total</Col > <Col>{calculatePrice(room, totalDays, { parents: data.parents, children: data.children || 0 })}</Col></Row>}
+                                        title={<Row justify='space-between'>
+                                            <Col>Total</Col>
+                                            <Col>{calculatePrice(room, totalDays, { parents: data.parents, children: data.children || 0 })}</Col>
+                                        </Row>}
                                         description={room.description}
                                     />
 
