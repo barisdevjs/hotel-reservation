3bc925620156c7d30649c71f8a8be1a101f6cece
Barış SAVAŞ <barissavas17@gmail.com>
2023-04-01 14:15:31 +0300

    types added

---
+++ src/utils/types.ts
@@ -0,0 +1,86 @@
+import dayjs, { Dayjs } from 'dayjs';
+
+
+export interface HotelListI  {
+    id: string,
+    hotel_name: string
+}
+
+export interface RoomI {
+    id:number,
+    title: string,
+    description: string,
+    photo:string,
+    price:number
+}
+
+export const initialRoom = {
+    id: 0,
+    title: '',
+    description: '',
+    photo: '',
+    price:0
+}
+
+export interface ScenicI  {
+    id:number,
+    title: string,
+    photo:string,
+    price_rate:number
+}
+
+export const initialScenic = {
+    id: 0,
+    title: '',
+    photo: '',
+    price_rate:0
+}
+
+export type FormT = {
+    id: string
+    parents: number
+    hotel_id:number
+    hotel_name: string
+    startDate: Dayjs
+    endDate: Dayjs
+    max_adult_size: number
+    child_status: boolean
+    children?:number | null
+    city?: string
+    possibilities?: string[]
+    room_type: RoomI[]
+    room_scenic: ScenicI[]
+    selectedRoom : RoomI
+    selected_scene : ScenicI
+  }
+
+export type HotelT = {
+    id: string,
+    hotel_id :number,
+    city: string,
+    possibilities: string[],
+    max_adult_size: number,
+    child_status: boolean,
+    room_type:RoomI,
+    room_scenic : ScenicI
+  }
+
+  export const initialHotel = {
+    id: '',
+    hotel_id: 0,
+    city: '',
+    possibilities:[],
+    max_adult_size:0,
+    child_status: false,
+    room_type: initialRoom,
+    room_scenic: initialScenic
+  }
+
+
+  export interface HotelProps {
+    data: FormT;
+    setData: React.Dispatch<React.SetStateAction<FormT>>;
+    updateFields: (fields: Partial<FormT>) => void;
+  }
+
+  
