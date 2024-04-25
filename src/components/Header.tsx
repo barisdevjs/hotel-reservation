4fc0622dd100ce51dd9f642d16757f23a200a315
Barış SAVAŞ <barissavas17@gmail.com>
2024-04-25 20:36:08 +0300

    Header.tsx reformatted

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
 function Header() {
-
-    return (
-        <header className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 rounded-b bg-blue-900 text-white'>
-            <h2 className='text-2xl'>Hotel Skyking </h2>
-            <button>New Reservation</button>
-        </header>
-    )
+  return (
+    <header className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 rounded-b bg-blue-900 text-white">
+      <h2 className="text-2xl">Hotel Skyking </h2>
+      <button>New Reservation</button>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
